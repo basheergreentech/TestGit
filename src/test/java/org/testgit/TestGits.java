ecd1f1e925fac4d9a30672ca21aec3af1aabb1cb
basheergreentech <basheergreentech@gmail.com>
2021-01-26 22:58:37 +0530

    Comment Added from Ahamed

--- src/test/java/org/testgit/TestGits.java
+++ src/test/java/org/testgit/TestGits.java
@@ -5,8 +5,8 @@ public class TestGits {
 	public static void main(String[] args) {
 		
 		System.out.println("Test1");
-		
-		System.out.println("Test2Ahamed 2ndTime Line Changed");
+		//Verify this 
+		System.out.println("Welcome");
 		
 	}
 	
