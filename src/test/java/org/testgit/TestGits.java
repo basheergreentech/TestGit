39fe2c00710ea707c9d85cd955fb1f98721fac58
basheergreentech <basheergreentech@gmail.com>
2021-01-26 22:52:25 +0530

    Ahamed 2ndTime Line Changed

--- src/test/java/org/testgit/TestGits.java
+++ src/test/java/org/testgit/TestGits.java
@@ -5,7 +5,8 @@ public class TestGits {
 	public static void main(String[] args) {
 		
 		System.out.println("Test1");
-		System.out.println("Test2Ahamed");
+		
+		System.out.println("Test2Ahamed 2ndTime Line Changed");
 		
 	}
 	
